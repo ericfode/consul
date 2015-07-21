90550ebe6fc3e634a24bd7fdda39ffae12421947
Eric Fode <ericfode@gmail.com>
2015-07-21 11:01:17 -0700

    Update consul.tf

--- terraform/aws/consul.tf
+++ terraform/aws/consul.tf
@@ -68,11 +68,5 @@ resource "aws_security_group" "consul" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    // This is for outbound internet access
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+
 }
